4d04a027b13dc70d1b574a7ba1d19f52b260b485
vi8249 <vi8249@gmail.com>
2017-06-13 12:21:07 +0800

    twelfth commit

--- app/src/main/java/com/example/vi8249/intelligentbasketballcourt/CourtTemperatureChartActivity.java
+++ app/src/main/java/com/example/vi8249/intelligentbasketballcourt/CourtTemperatureChartActivity.java
@@ -4,5 +4,5 @@ package com.example.vi8249.intelligentbasketballcourt;
  * Created by vi8249 on 2017/6/13.
  */
 
-public class TemperatureData {
+public class CourtTemperatureChartActivity {
 }
--- app/src/main/java/com/example/vi8249/intelligentbasketballcourt/HumidityData.java
+++ app/src/main/java/com/example/vi8249/intelligentbasketballcourt/HumidityData.java
@@ -1,43 +1,8 @@
 package com.example.vi8249.intelligentbasketballcourt;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
 /**
- * Created by vi8249 on 2017/6/12.
+ * Created by vi8249 on 2017/6/13.
  */
 
-public class HumidityData implements Parcelable {
-    public static final Parcelable.Creator<HumidityData> CREATOR = new Parcelable.Creator<HumidityData>() {
-        public HumidityData createFromParcel(Parcel in) {
-            return new HumidityData(in);
-        }
-
-        public HumidityData[] newArray(int size) {
-            return new HumidityData[size];
-
-        }
-    };
-    String timestamp;
-    float data;
-
-    protected HumidityData(Parcel in) {
-        timestamp = in.readString();
-        data = in.readFloat();
-    }
-
-    public HumidityData() {
-
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(timestamp);
-        dest.writeFloat(data);
-    }
+public class HumidityData {
 }
